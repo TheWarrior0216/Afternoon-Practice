cae4314f3bf742d7f6439e3d5774655921b5ae3c
Seth Baxter <TheWarrior0216@gmail.com>
2024-06-10 12:47:24 -0600

    messed with some js

---
+++ app.js
@@ -0,0 +1,20 @@
+let HomePoints = 0
+let AwayPoints = 0
+
+function reset(){
+  HomePoints =0
+  AwayPoints =0
+}
+
+function addHPoints(){
+HomePoints ++
+}
+function addHPoints3(){
+HomePoints += 3
+}
+function addAPoints(){
+AwayPoints ++
+}
+function addAPoints3(){
+AwayPoints += 3
+}
