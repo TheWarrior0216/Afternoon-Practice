5a947a4c88998a089c67cfa2e695d61aeda00587
Seth Baxter <TheWarrior0216@gmail.com>
2024-06-11 09:21:12 -0600

    Finished

--- app.js
+++ app.js
@@ -4,17 +4,38 @@ let AwayPoints = 0
 function reset(){
   HomePoints =0
   AwayPoints =0
+  UpdateAPoints()
+  UpdateHPoints()
 }
 
 function addHPoints(){
-HomePoints ++
+HomePoints ++ ;
+console.log(HomePoints)
+UpdateHPoints()
 }
 function addHPoints3(){
 HomePoints += 3
+console.log(HomePoints)
+UpdateHPoints()
 }
 function addAPoints(){
 AwayPoints ++
+console.log(AwayPoints)
+UpdateAPoints()
 }
 function addAPoints3(){
 AwayPoints += 3
+console.log(AwayPoints)
+UpdateAPoints()
+}
+
+function UpdateHPoints(){
+  const HPoints = document.getElementById('HomeScore')
+  console.log('here is the rsult box element', HPoints);
+HPoints.innerText = HomePoints
+}
+function UpdateAPoints(){
+  const APoints = document.getElementById('AwayScore')
+  console.log('here is the rsult box element', APoints);
+APoints.innerText = AwayPoints
 }
